748c20479203068fd34cb288ee1a0e57fe1f6c8c
OillerSDev <oiller.mulava@aviorlogistica.com.br>
2022-12-26 17:03:58 -0300

    Finazilado sem opinião da instrutora

---
+++ 3DaysOfCode.js
@@ -0,0 +1,29 @@
+
+alert('Para proseguir ou pular estapas pressione a tecla "Enter".')
+
+
+const QueryOfArea = prompt('Você deseja seguir qual área? Digite 1 Para Back-End e 2 para Front-End.')
+
+ 
+if (QueryOfArea == 1){
+    alert('Se estiver na área de Back-End, poderá aprender C# ou aprender Java.');
+
+}
+else if (QueryOfArea == 2) {
+    alert('Se estiver na área de Front-End, se quer aprender React ou aprender Vue.'); 
+
+}
+
+const aprofundar = prompt('Se aprofundar nessa área digite 1')
+const FullStack = prompt('Se tornar FullStack digite 2')
+
+
+
+    for (let questões = 0; questões < 5; questões++){
+        prompt('Tem mais alguma tecnologia que você gostaria de aprender?')
+            
+         }
+    
+alert('Siga os passos do curso e bons estudos.')
+
+         
